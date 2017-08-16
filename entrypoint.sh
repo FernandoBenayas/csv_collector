25bc95ea1a5e51809c7378a052b68a7e9c7ba1f3
fbenayas <f.benayas@alumnos.upm.es>
2017-08-16 11:48:03 +0200

    Changed elasticsearch url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 echo Checking if elasticsearch is up and listening...
-ES_URL="http://localhost:9200/_cat/indices?v"
+ES_URL="http://elasticsearch:9200/_cat/indices?v"
 es_request=$(curl $ES_URL)
 
-while [[ $es_request != *"simulation"* ]]
-do
-es_request=$(curl $ES_URL)
-echo Elasticsearch is still sleeping...
-/bin/sleep 2
+while [[ $es_request != *"simulation"* ]]; do
+	es_request=$(curl $ES_URL)
+	echo Elasticsearch is still sleeping...
+	/bin/sleep 5
 done
 
 echo You used pókeflute. Elasticsearch just woke up.
